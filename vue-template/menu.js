d327fff52e116d0834fa12ab2e060ac28126f0f6
nanamwu <166733255+nanamwu@users.noreply.github.com>
2024-06-14 09:02:42 +0900

    就職申込画面追加

--- vue-template/menu.js
+++ vue-template/menu.js
@@ -1,22 +1,22 @@
-'use strict';
+"use strict";
 
 PetiteVue.createApp({
-  username: '',
-  unread: '',
-  data: [],
+  username: "", //空の文字列
+  unread: "", //空の文字列
+  data: [], //空の配列
   async init() {
     const username = sessionStorage.username;
     if (!username) {
-      window.alert('ログインしてください');
-      location.href = 'login.html';
+      window.alert("ログインしてください");
+      location.href = "login.html";
     }
     this.username = username;
 
-    const res = await fetch('data.json');
+    const res = await fetch("data.json");
     const obj = await res.json();
     this.data = obj.list;
     console.log(this.data);
 
     this.unread = this.data.length;
-  }
+  },
 }).mount();
