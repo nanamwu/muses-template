577860fc53a530d6e2a9c11c6d7fe11c5dfdd63c
nanamwu <166733255+nanamwu@users.noreply.github.com>
2024-07-05 11:57:44 +0900

    7/52回目

--- vue-template/end.js
+++ vue-template/end.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const selectedDropdown = localStorage.getItem("selectedDropdown");
   const selectedRadio = localStorage.getItem("selectedRadio");
+
   if (selectedDropdown) {
     document.getElementById(
       "displayDropdown"
-    ).textContent = `選択されたドロップダウン: ${selectedDropdown}`;
+    ).textContent = `${selectedDropdown}`;
   } else {
     document.getElementById("displayDropdown").textContent =
       "選択されたドロップダウンはありません。";
   }
+
   if (selectedRadio) {
-    document.getElementById(
-      "displayRadio"
-    ).textContent = `選択されたラジオボタン: ${selectedRadio}`;
+    document.getElementById("displayRadio").textContent = `${selectedRadio}`;
   } else {
     document.getElementById("displayRadio").textContent =
       "選択されたラジオボタンはありません。";
